5f516fe70d5a87d4bc8170038d139cd7d4e7890d
Jose  Luis <josvarg2014@gmail.com>
2016-07-22 12:10:28 -0500

    html

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,4 @@
+<<<<<<< 213b362eafae6322729371d08eec26d3d2089ddb
 $( document ).ready(function() {
                     var $server;
                     $server = 'http://localhost:1280/xampp/weatherApp/www/';
@@ -45,3 +46,30 @@ $( document ).ready(function() {
                  AgregandoImg();
 
             });
+=======
+/*
+ * Please see the included README.md file for license terms and conditions.
+ */
+
+
+// This file is a suggested starting place for your code.
+// It is completely optional and not required.
+// Note the reference that includes it in the index.html file.
+
+
+/*jslint browser:true, devel:true, white:true, vars:true */
+/*global $:false, intel:false app:false, dev:false, cordova:false */
+
+
+
+// This file contains your event handlers, the center of your application.
+// NOTE: see app.initEvents() in init-app.js for event handler initialization code.
+
+// function myEventHandler() {
+//     "use strict" ;
+// // ...event handler code here...
+// }
+
+
+// ...additional event handlers here...
+>>>>>>> Creando Formulario
